5d2ef1940f5e53bed5eeb2d3374cb0f418f2fe5d
sandra04 <sandradelaunay@hotmail.com>
2017-04-19 11:02:18 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+// Connexion bases de données
+		$pdo = new PDO('mysql:host=localhost;dbname=mike', 'root', '', array(
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+		));
+
+		// Requête SQL
+		$resultat = $pdo -> prepare("SHOW DATABASES;");
+		$resultat -> execute();
+
+		// Tri de la requete
+		$dataBase = $resultat->fetchAll(PDO::FETCH_ASSOC);
+
+
+?>
+
+
 <html>
 	<head>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -5,21 +24,62 @@
 
 	<body>
 		<div id="mike">
+			<form>
+				<select id="databaseSelect">
+
+					<?php foreach($dataBase as $key=>$value){
+
+						echo '<option value="'. $value['Database'].'">' . $value['Database'] . '</option>';
+					}
+					?>
+				</select><br/>
+				
+				<fieldset>
+					<legend>Requete</legend>
+
+					<textarea name="sql" id="sql" rows="4" cols="50"></textarea>
+					<br/>
+					<input type="submit" value="Envoyer"/>
+				</fieldset>
+			</form>
 		</div>
 
 		<script>
-			var request = $.ajax({
-			  url: "read.php",
-			  method: "POST"
+
+			$(function(){
+				$("input").click(function(e){
+					// Annulation de l'actualisation de la page
+					e.preventDefault();
+					console.log("Mike")
+
+
+					// Récupération de la valeur de notre textarea
+					var myRequest = $("#sql").val();
+
+					var dataBase = $("#databaseSelect").val();
+
+
+					// Requête AJAX - Envoi des infos du formulaire
+					var request = $.ajax({
+					  url: "read2.php", // page de la requête
+					  method: "POST", // méthode de la requête
+					  data:{Requet : myRequest, data: dataBase} // data envoyée à la page
+					});
+					
+					request.done(function( msg ) {
+					  $( "#mike" ).html( msg );
+					  $( "#requet" ).html( myRequest );
+					});
+					 
+					request.fail(function( jqXHR, textStatus ) {
+					  alert( "Request failed: " + textStatus );
+					});
+						
+				});
 			});
-			 
-			request.done(function( msg ) {
-			  $( "#mike" ).html( msg );
-			});
-			 
-			request.fail(function( jqXHR, textStatus ) {
-			  alert( "Request failed: " + textStatus );
-			});
+
+
+			
 		</script>
 	</body>
 </html>
